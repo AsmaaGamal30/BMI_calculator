4336d5b88365373f572f2712853ab089fae17ceb
Asmaa Gamal <asmagamal311@gmail.com>
2022-01-28 02:35:30 +0200

    initial commit

---
+++ lib/modules/bmi_app/bmi_result/BMI_result_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class BMIResultScreen extends StatelessWidget {
+  final bool isMale;
+  final int result;
+  final int age;
+  BMIResultScreen({
+   required this.result,
+   required this.age,
+   required this.isMale,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI Result',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Gender : ${isMale ? 'Male' : 'Female'}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+              ),
+            ),
+            Text(
+              'Result : $result',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+              ),
+            ),
+            Text(
+              'Age : $age',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
